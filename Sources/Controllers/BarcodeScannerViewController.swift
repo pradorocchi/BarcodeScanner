0c3b9ee99ab972b4016f2e38078a218be213ca23
Vencel Kátai <vencelster@gmail.com>
2018-05-12 21:56:04 +0200

    making footerHeight public

--- Sources/Controllers/BarcodeScannerViewController.swift
+++ Sources/Controllers/BarcodeScannerViewController.swift
@@ -32,7 +32,7 @@ public protocol BarcodeScannerDismissalDelegate: class {
  - Not found error message
  */
 open class BarcodeScannerViewController: UIViewController {
-  private static let footerHeight: CGFloat = 75
+  static let footerHeight: CGFloat = 75
 
   // MARK: - Public properties
 
